6288b50f137c65a88b492639d907f18fe75043c3
Jixin Liu <187157249@qq.com>
2019-01-16 23:50:06 +0800

    11

--- src/main/java/com/pawpaw/framework/common/concurrent/ConcurrentCall.java
+++ src/main/java/com/pawpaw/framework/common/concurrent/ConcurrentCall.java
@@ -1,40 +1,35 @@
 package com.pawpaw.framework.common.concurrent;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 
-/**
- * every call should implements this abstract class
- * 
- * @author liujixin
- *
- * @param <V>
- */
+
 public abstract class ConcurrentCall<V> implements Callable<V> {
-	Logger logger = LoggerFactory.getLogger(com.pawpaw.common.concurrent.ConcurrentCall.class);
-	private CountDownLatch cdl;
+    Logger logger = LoggerFactory.getLogger(ConcurrentCall.class);
+    private CountDownLatch cdl;
 
-	protected void setCdl(CountDownLatch cdl) {
-		this.cdl = cdl;
-	}
+    protected void setCdl(CountDownLatch cdl) {
+        this.cdl = cdl;
+    }
 
-	@Override
-	public V call() throws Exception {
-		try {
-			V v = this.doCall();
-			return v;
-		} catch (Throwable t) {
-			logger.error("exception happends,{}", t.getMessage());
-			throw new RuntimeException(t);
-		} finally {
-			this.cdl.countDown();
-		}
+    @Override
+    public V call() throws Exception {
+        try {
+            V v = this.doCall();
+            return v;
+        } catch (Throwable t) {
+            logger.error("exception happends,{}", t.getMessage());
+            throw new RuntimeException(t);
+        } finally {
+            this.cdl.countDown();
+        }
 
-	}
+    }
 
-	public abstract V doCall();
+    public abstract V doCall();
 
 }
--- src/main/java/com/pawpaw/framework/common/concurrent/ConcurrentExecutor.java
+++ src/main/java/com/pawpaw/framework/common/concurrent/ConcurrentExecutor.java
@@ -1,106 +1,113 @@
 package com.pawpaw.framework.common.concurrent;
 
-import com.pawpaw.common.concurrent.ConcurrentCall;
-import com.pawpaw.common.concurrent.ConcurrentExecuteResult;
-import com.pawpaw.common.util.CollectionConventer;
+import com.pawpaw.framework.common.util.CollectionConventer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.security.InvalidParameterException;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.*;
 
 /**
- * concurrent executor,the main thread continues while all child (call or
- * thread) finish or fail(exception happens)
+ * 并发执行的任务执行器，当所有现成都执行完毕的时候，才会进行主线程
+ *
+ * @Auther: liujixin
+ * @Date: 2018-12-25
  */
+
 public class ConcurrentExecutor {
 
-	private static final Logger logger = LoggerFactory.getLogger(com.pawpaw.common.concurrent.ConcurrentExecutor.class);
+    private static final Logger logger = LoggerFactory.getLogger(ConcurrentExecutor.class);
 
-	private static final ExecutorService executor = Executors.newCachedThreadPool();
+    private ExecutorService executor;
 
-	public static <V> ConcurrentExecuteResult<V> execute(Collection<ConcurrentCall<V>> calls) {
-		return execute(calls, 0L);
 
-	}
+    public ConcurrentExecutor(ExecutorService executor) {
+        this.executor = executor;
+    }
 
-	/**
-	 * execute calls parallelly.
-	 *
-	 * @param calls
-	 * @param timeOut  in millionseconds
-	 *            timeout in millions the main thread blocks.
-	 */
-	public static <V> ConcurrentExecuteResult<V> execute(Collection<ConcurrentCall<V>> calls, long timeOut) {
-		logger.debug("开始多线程任务，子任务数量:{},time out is {}", calls.size(), timeOut);
-		if (timeOut < 0) {
-			throw new InvalidParameterException("time out must greate or equals than zero");
-		}
-		ConcurrentExecuteResult<V> result = new ConcurrentExecuteResult<V>();
-		if (calls == null || calls.size() == 0) {
-			return result;
-		}
-		CountDownLatch cdl = new CountDownLatch(calls.size());
-		List<Future<V>> futures = new LinkedList<>();
-		for (ConcurrentCall<V> call : calls) {
-			call.setCdl(cdl);
-			Future<V> future = executor.submit(call);
-			futures.add(future);
-		}
-		try {
-			logger.debug("wait sub thread finish...");
-			if (timeOut == 0) {
-				cdl.await();
-			} else {
-				cdl.await(timeOut, TimeUnit.MILLISECONDS);
-			}
-			logger.debug("all sub thread finish...begin main thread");
-			for (Future<V> f : futures) {
-				try {
-					result.addCompleteResult(f.get(timeOut, TimeUnit.MILLISECONDS));
-				} catch (ExecutionException | TimeoutException e) {
-					result.addException(e);
-				}
-			}
-			return result;
-		} catch (InterruptedException e) {
-			logger.warn("error happends on main thread,{}", e);
-			throw new RuntimeException(e);
-		}
+    public <V> ConcurrentExecuteResult<V> execute(Collection<ConcurrentCall<V>> calls) {
+        return execute(calls, 0L);
 
-	}
+    }
 
-	public static <V> ConcurrentExecuteResult<V> executeBatch(Collection<ConcurrentCall<V>> calls, int batchSize) {
-		return executeBatch(calls, batchSize, 0L);
-	}
+    /**
+     * 并发执行子任务，所有任务都返回后，返回响应
+     *
+     * @param calls
+     * @param timeOut in millionseconds
+     *                timeout in millions the main thread blocks.
+     */
+    public <V> ConcurrentExecuteResult<V> execute(Collection<ConcurrentCall<V>> calls, long timeOut) {
+        logger.debug("开始多线程任务，子任务数量:{},time out is {}", calls.size(), timeOut);
+        if (timeOut < 0) {
+            throw new IllegalArgumentException("time out must greate or equals than zero");
+        }
+        ConcurrentExecuteResult<V> result = new ConcurrentExecuteResult<V>(calls.size());
+        if (calls == null || calls.size() == 0) {
+            return result;
+        }
+        CountDownLatch cdl = new CountDownLatch(calls.size());
+        List<Future<V>> futures = new LinkedList<>();
+        for (ConcurrentCall<V> call : calls) {
+            call.setCdl(cdl);
+            Future<V> future = executor.submit(call);
+            futures.add(future);
+        }
+        try {
+            logger.debug("wait sub thread finish...");
+            boolean finishBeforeTimeElipse = true;
+            if (timeOut == 0) {
+                cdl.await();
+            } else {
+                finishBeforeTimeElipse = cdl.await(timeOut, TimeUnit.MILLISECONDS);
+            }
+            result.setMainThreadWaitTimeOut(!finishBeforeTimeElipse);
+            logger.debug("all sub thread finish...begin main thread");
+            for (Future<V> f : futures) {
+                try {
+                    result.addCompleteCallResult(f.get(timeOut, TimeUnit.MILLISECONDS));
+                } catch (ExecutionException | TimeoutException e) {
+                    result.addFailCallResult(e);
+                }
+            }
+            return result;
+        } catch (InterruptedException e) {
+            logger.warn("error happends on main thread,{}", e);
+            throw new RuntimeException(e);
+        }
 
-	/**
-	 * calls will be seperated into a few of batch. batchs are executed
-	 * sequentially.
-	 *
-	 * @param calls
-	 * @param batchSize
-	 * @return results
-	 */
-	public static <V> ConcurrentExecuteResult<V> executeBatch(Collection<ConcurrentCall<V>> calls, int batchSize,
-                                                              long timeOut) {
-		logger.debug("begin concurrent calls，all size:{},batch size:{}", calls.size(), batchSize);
-		if (calls == null || calls.size() == 0) {
-			return new ConcurrentExecuteResult<V>();
-		}
-		ConcurrentExecuteResult<V> finalResult = new ConcurrentExecuteResult<>();
-		List<List<ConcurrentCall<V>>> deviceCalls = CollectionConventer.devide(calls, batchSize);
-		for (List<ConcurrentCall<V>> batch : deviceCalls) {
-			ConcurrentExecuteResult<V> batchResult = execute(batch, timeOut);
-			finalResult.merge(batchResult);
-		}
-		return finalResult;
-	}
+    }
 
-	////////////////////////////////////////////////////////
-	////////////////////////////////////////////////////////
+    public <V> ConcurrentExecuteResult<V> executeBatch(Collection<ConcurrentCall<V>> calls, int batchSize) {
+        return executeBatch(calls, batchSize, 0L);
+    }
+
+    /**
+     * calls will be seperated into a few of batch. batchs are executed
+     * sequentially.
+     *
+     * @param calls
+     * @param batchSize
+     * @return results
+     */
+    public <V> ConcurrentExecuteResult<V> executeBatch(Collection<ConcurrentCall<V>> calls, int batchSize,
+                                                       long timeOut) {
+        logger.debug("begin concurrent calls，all size:{},batch size:{}", calls.size(), batchSize);
+        if (calls == null || calls.size() == 0) {
+            return new ConcurrentExecuteResult<V>(calls.size());
+        }
+        ConcurrentExecuteResult<V> finalResult = new ConcurrentExecuteResult<>(calls.size());
+        List<List<ConcurrentCall<V>>> deviceCalls = CollectionConventer.devide(calls, batchSize);
+        for (List<ConcurrentCall<V>> batch : deviceCalls) {
+            ConcurrentExecuteResult<V> batchResult = execute(batch, timeOut);
+            finalResult.merge(batchResult);
+        }
+        return finalResult;
+    }
+
+    ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
 
 }
