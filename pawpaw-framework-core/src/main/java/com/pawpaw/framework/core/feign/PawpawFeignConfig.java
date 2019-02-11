d2c6e0dbc2680886afd4a33607e02c408af7894d
Jixin Liu <187157249@qq.com>
2019-02-11 21:39:07 +0800

    no message

--- pawpaw-framework-core/src/main/java/com/pawpaw/framework/core/feign/PawpawFeignConfig.java
+++ pawpaw-framework-core/src/main/java/com/pawpaw/framework/core/feign/PawpawFeignConfig.java
@@ -1,6 +1,5 @@
 package com.pawpaw.framework.core.feign;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.appinfo.EurekaInstanceConfig;
 import com.pawpaw.framework.core.factory.json.ObjectMapperFactory;
 import com.pawpaw.framework.core.factory.json.PawpawObjectMapper;
@@ -9,23 +8,12 @@ import feign.codec.Encoder;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 public class PawpawFeignConfig {
     @Autowired
     private ObjectFactory<HttpMessageConverters> messageConverters;
 
-    @Autowired
-    @Bean
-    public FeignRequestHeader getFeignRequestHeader(EurekaInstanceConfig eurekaInstanceConfig) {
-        String ip = eurekaInstanceConfig.getIpAddress();
-        int port = eurekaInstanceConfig.getNonSecurePort();
-        String hostName = eurekaInstanceConfig.getHostName(true);
-        String appName = eurekaInstanceConfig.getAppname();
-        return new FeignRequestHeader(appName, ip, String.valueOf(port), hostName);
-    }
 
     @Bean
     public Decoder getDecoder() {
@@ -35,7 +23,12 @@ public class PawpawFeignConfig {
 
     @Bean
     @Autowired
-    public Encoder getEncoder(FeignRequestHeader requestHeader) {
+    public Encoder getEncoder(EurekaInstanceConfig eurekaInstanceConfig) {
+        String ip = eurekaInstanceConfig.getIpAddress();
+        int port = eurekaInstanceConfig.getNonSecurePort();
+        String hostName = eurekaInstanceConfig.getHostName(true);
+        String appName = eurekaInstanceConfig.getAppname();
+        FeignRequestHeader requestHeader = new FeignRequestHeader(appName, ip, String.valueOf(port), hostName);
         return new DefaultPawpawFeignEncoder(this.messageConverters, requestHeader.getHeaders());
     }
 
