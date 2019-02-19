46162e8febf64995d6ba728b56a8a6873ba1a8b4
liujixin <liujixin@redefine.global>
2019-02-19 12:02:20 +0800

    111

--- pawpaw-framework-core/src/main/java/com/pawpaw/framework/core/common/util/SHAUtil.java
+++ pawpaw-framework-core/src/main/java/com/pawpaw/framework/core/common/util/SHAUtil.java
@@ -1,33 +1,39 @@
 package com.pawpaw.framework.core.common.util;
 
-import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.bouncycastle.crypto.RuntimeCryptoException;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.Mac;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+import java.security.Security;
 
 public class SHAUtil {
 
-	public static String SHA256Str(String str) {
-		try {
-			String hex = DigestUtils.sha256Hex(str);
-			return hex;
-		} catch (Exception e) {
-			throw new RuntimeException("cal SHA256 fail");
-		}
-	}
+    public static String SHA256Str(String str) {
+        try {
+            String hex = DigestUtils.sha256Hex(str);
+            return hex;
+        } catch (Exception e) {
+            throw new RuntimeException("cal SHA256 fail");
+        }
+    }
 
-	public static String HMACSHA256(String secret, String str) {
-		try {
-			Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
-			SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
-			sha256_HMAC.init(secret_key);
-			byte[] data = sha256_HMAC.doFinal(str.getBytes("utf-8"));
-			String hash = Hex.encodeHexString(data);
-			return hash;
-		} catch (Exception e) {
-			throw new RuntimeException("cal HMACSHA256 fail");
-		}
-	}
+    private static String encrytSHA256(String content, String secret) {
+        try {
+            Security.addProvider(new BouncyCastleProvider());
+            SecretKey secretKey = new SecretKeySpec(secret.getBytes("UTF8"),
+                    "HmacSHA256");
+            Mac mac = Mac.getInstance(secretKey.getAlgorithm());
+            mac.init(secretKey);
+            byte[] digest = mac.doFinal(content.getBytes("UTF-8"));
+            return new HexBinaryAdapter().marshal(digest);
+        } catch (Exception e) {
+            throw new RuntimeCryptoException("加密异常");
+        }
+
+    }
 
 }
